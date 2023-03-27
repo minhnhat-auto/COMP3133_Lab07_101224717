7bf976042aa27d6615ba855b842d600179f96361
Nhat Minh Vo <nhat@Nhats-MacBook-Pro.local>
2023-03-26 23:57:33 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Pipe } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';// <-- NgModel lives here
+import { RemoveSpacesPipe } from './remove-spaces.pipe';
+import { InputFormatDirective } from './input-format.directive';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    RemoveSpacesPipe,
+    InputFormatDirective,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
